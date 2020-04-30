8bd357b969e399858f08f230538397cf88251477
FabricioBernardes <FabricioBernardes@users.noreply.github.com>
2020-04-30 16:44:35 -0300

    Updates

---
+++ precache-manifest.db58131ebe9e1c4a69a7190f8ca2e623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b07964c0f54e7d9363d550222cd129",
+    "url": "/recatalogo-preenchimento/index.html"
+  },
+  {
+    "revision": "5f894fdebd923a8a30a6",
+    "url": "/recatalogo-preenchimento/static/css/main.5f6f4cbf.chunk.css"
+  },
+  {
+    "revision": "d918b03bef2d449e14ad",
+    "url": "/recatalogo-preenchimento/static/js/2.82fe41c5.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/recatalogo-preenchimento/static/js/2.82fe41c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f894fdebd923a8a30a6",
+    "url": "/recatalogo-preenchimento/static/js/main.33c12e91.chunk.js"
+  },
+  {
+    "revision": "ec96cf9923b734eb14f0",
+    "url": "/recatalogo-preenchimento/static/js/runtime-main.1816d0c8.js"
+  }
+]);
